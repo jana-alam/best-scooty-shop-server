5af231e03e0ebbda1f466b3efff2572621666961
jana-alam <janaalam2929@gmail.com>
2021-11-13 01:36:20 +0600

    cors added

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+
+// middleware
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "Server Running" });
