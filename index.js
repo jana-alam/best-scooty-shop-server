67fcf5b65ea6e85721747b445e8b9e0462dd85fa
jana-alam <janaalam2929@gmail.com>
2021-11-13 16:23:03 +0600

    product post & get

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function run() {
     await client.connect();
     const database = client.db("scooty_shop");
     const userCollection = database.collection("users");
+    const productCollection = database.collection("products");
 
     // USER POST
     app.post("/user", async (req, res) => {
@@ -31,12 +32,38 @@ async function run() {
       res.json(result);
     });
     // Make Admin
-
     app.put("/user/admin", async (req, res) => {
       const email = req.body.email;
       const filter = { email: email };
       const updateDoc = { $set: { role: "admin" } };
       const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+    // checking user role
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      let admin = false;
+      if (result?.role === "admin") {
+        admin = true;
+      }
+      res.json({ admin: admin });
+    });
+    // get product
+    app.get("/products", async (req, res) => {
+      const cursor = productCollection.find({});
+      const products = await cursor.toArray();
+      console.log(products.length);
+      res.json(products);
+    });
+
+    // Add Product POST
+    app.post("/product", async (req, res) => {
+      const product = req.body;
+      product.stock = "In Stock";
+      const result = await productCollection.insertOne(product);
+      console.log(result);
       res.json(result);
     });
   } finally {
