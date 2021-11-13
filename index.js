1cb8b7fc074c233a2970474f70834434374bb01f
jana-alam <janaalam2929@gmail.com>
2021-11-13 09:20:13 +0600

    made admin api

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
 const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 // middleware
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@janaalam.ewacz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("scooty_shop");
+    const userCollection = database.collection("users");
+
+    // USER POST
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      console.log("user in db");
+      res.json(result);
+    });
+    // Make Admin
+
+    app.put("/user/admin", async (req, res) => {
+      const email = req.body.email;
+      const filter = { email: email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+  } finally {
+    // await client.connect();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.json({ message: "Server Running" });
